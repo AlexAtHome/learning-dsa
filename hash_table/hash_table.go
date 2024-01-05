d5677bca3eb5b178d659d7e0bd2162dc838b0910
Alex Bolotskov <mamoruuu@proton.me>
2024-01-05 12:38:35 +0600

    hash table done right (sorta)

--- hash_table/hash_table.go
+++ hash_table/hash_table.go
@@ -1,58 +1,97 @@
 package hashtable
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type Pair struct {
 	k int
 	v string
 }
 
+func (p *Pair) String() string {
+	return p.v
+}
+
 type HashTable struct {
-	Capacity int
+	capacity int
 	Size     int
-	list     map[int]Pair
+	list     []*Pair
 }
 
 func CreateHashTable(capacity int) *HashTable {
-	list := make(map[int]Pair)
+	list := make([]*Pair, capacity, capacity)
 	return &HashTable{capacity, 0, list}
 }
 
 func (table *HashTable) String() (output string) {
-	output = "[HashTable: "
-	i := 0
-	for _, v := range table.list {
+	output = "[HashTable "
+	for i, v := range table.list {
 		if i > 0 {
 			output += ", "
 		}
-		output += v.v
-		i++
+		if v != nil {
+			output += fmt.Sprintf("%v: %v", i, v)
+		} else {
+			output += fmt.Sprintf("%v: ---", i)
+		}
 	}
 	output += "]"
 	return output
 }
 
-func (table *HashTable) hash(key int) (index int) {
+func (table *HashTable) hash(key int) int {
 	hashConstant := 0.6180339887498949
-	size := float64(table.Capacity)
 	n := float64(key) * hashConstant
-	index = int(math.Floor(size * n))
-	return index
+	return int(math.Floor(n)) % table.capacity
 }
 
-func (table *HashTable) Insert(key int, value string) {
-	if table.Size == table.Capacity {
-		return
-	}
+func (table *HashTable) getIndexFor(key int) (int, error) {
 	index := table.hash(key)
-	table.Size++
-	table.list[index] = Pair{key, value}
+	item := table.list[index]
+	i := 1
+	for item != nil && item.k != key {
+		// recalculating the index, using double hashing technique
+		index = (index + i*table.hash(key)) % table.capacity
+		i++
+		if i == table.capacity {
+			return -1, fmt.Errorf("Out of space!")
+		}
+	}
+	return index, nil
 }
 
-func (table *HashTable) Delete(key int) {
-	delete(table.list, table.hash(key))
-	if table.Size == 0 {
-		return
+func (table *HashTable) Insert(key int, value string) bool {
+	if table.Size == table.capacity {
+		return false
 	}
-	table.Size = int(math.Max(float64(table.Size)-1, 0.0))
+	index, err := table.getIndexFor(key)
+	if err == nil {
+		table.list[index] = &Pair{key, value}
+		table.Size++
+		return true
+	} else {
+		return false
+	}
+}
+
+func (table *HashTable) Delete(key int) bool {
+	if table.Size <= 0 {
+		return false
+	}
+	index, _ := table.getIndexFor(key)
+	table.list[index] = nil
+	table.Size = int(math.Max(float64(table.Size-1), 0.0))
+	return true
+}
+
+func (table *HashTable) Search(key int) string {
+	index, _ := table.getIndexFor(key)
+	found := table.list[index]
+	if found == nil {
+		return ""
+	} else {
+		return found.v
+	}
 }
--- hash_table/hash_table_test.go
+++ hash_table/hash_table_test.go
@@ -11,8 +11,32 @@ func TestHashTableInsert(t *testing.T) {
 	table.Insert(111, "foo")
 	table.Insert(113, "bar")
 	table.Insert(123, "baz")
-	if table.Size != 3 {
-		t.Fatalf("Expected the size of hash table of 3, got %v", table.Size)
+	size := table.Size
+	if size != 3 {
+		t.Fatalf("Expected the size of hash table of 3, got %v", size)
+	}
+}
+
+func TestHashTableHashCollision(t *testing.T) {
+	table := ht.CreateHashTable(1000)
+	table.Insert(10, "foo")
+	table.Insert(20, "bar")
+	table.Insert(30, "baz")
+	size := table.Size
+	if size != 3 {
+		t.Fatalf("Expected the size of hash table of 3, got %v", size)
+	}
+}
+
+func TestHashTableInsertOverflow(t *testing.T) {
+	table := ht.CreateHashTable(3)
+	table.Insert(112, "foo")
+	table.Insert(222, "bar")
+	table.Insert(3333, "baz")
+	table.Insert(3454, "bor")
+	size := table.Size
+	if size != 3 {
+		t.Fatalf("Expected the size of hash table of 3, got %v", size)
 	}
 }
 
@@ -20,15 +44,35 @@ func TestHashTableDelete(t *testing.T) {
 	table := ht.CreateHashTable(10)
 	table.Insert(111, "foo")
 	table.Delete(111)
-	if table.Size != 0 {
-		t.Fatalf("Expected the hash table to be empty, got thge size of %v", table.Size)
+	size := table.Size
+	if size != 0 {
+		t.Fatalf("Expected the hash table to be empty, got thge size of %v", size)
 	}
 
 	table.Insert(111, "foo")
 	table.Insert(112, "bar")
 	table.Insert(122, "baz")
 	table.Delete(112)
-	if table.Size != 2 {
-		t.Fatalf("Expected the size of hash table of 2, got %v", table.Size)
+	size = table.Size
+	if size != 2 {
+		t.Fatalf("Expected the size of hash table of 2, got %v", size)
+	}
+}
+
+func TestHashTableDeleteUnderflow(t *testing.T) {
+	table := ht.CreateHashTable(10)
+	result := table.Delete(111)
+	if result == true {
+		t.Fatalf("Expected the deletion operation to return false, got %v", result)
+	}
+}
+
+func TestHashTableSearch(t *testing.T) {
+	table := ht.CreateHashTable(10)
+	table.Insert(123, "foo")
+	table.Insert(234, "bar")
+	found := table.Search(123)
+	if found != "foo" {
+		t.Fatalf("Expected the found value to be \"foo\", got \"%v\"", found)
 	}
 }
